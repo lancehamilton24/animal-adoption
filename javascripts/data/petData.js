0c6ae7b8565459464e482540da21806cb89c9b0b
Lance Hamilton <lance.hamilton24@gmail.com>
2018-10-05 12:59:43 -0500

    added object data to pets.json

--- javascripts/data/petData.js
+++ javascripts/data/petData.js
@@ -1,1 +1,25 @@
-console.log('pet data')
+import {setPets} from '../components/petComponent.js';
+function executeThisCodeAfterFileLoaded () {
+    const data = JSON.parse(this.responseText);
+    setPets(data.pets);
+}
+
+function executeThisCodeifXhrFails () {
+    console.log('Looks broken');
+
+}
+
+
+
+
+const getPets = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load', executeThisCodeAfterFileLoaded);
+    myRequest.addEventListener('error', executeThisCodeifXhrFails);
+    myRequest.open('GET', './db/characters.json');
+    myRequest.send();
+};
+
+console.log(getPets);
+
+export {getPets};
