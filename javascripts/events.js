521515bc70b9eb7908328151be957c61dfa2d0cc
Lance Hamilton <lance.hamilton24@gmail.com>
2018-10-06 10:57:29 -0500

    added sortEvents function

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,1 +1,10 @@
-console.log('events.js')
+const sortEvents = () => {
+    const catButton = document.getElementById('catButton');
+    const dogButton = document.getElementById('dogButton');
+    const dinoButton = document.getElementById('dinoButton');
+    const allButton = doucment.getElementById('all');
+    catButton.addEventListener('click', sortPets);
+    dogButton.addEventListener('click', sortPets);
+    dinoButton.addEventListener('click', sortPets);
+    allButton.addEventListener('click', sortPets);
+};
